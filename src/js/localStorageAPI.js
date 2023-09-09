ca75d79d11678232165447f77aacaa17a427c6ab
Dmytro Shkatula <idnol84@gmail.com>
2023-09-09 16:01:11 +0300

    part-1

--- src/js/localStorageAPI.js
+++ src/js/localStorageAPI.js
@@ -1,6 +1,11 @@
-export function addCart(cart) {
-    const carts = JSON.parse(localStorage.getItem('cartsKey')) ?? [];
-    carts.push(cart);
-    localStorage.setItem('cartsKey', JSON.stringify(carts));
+const API_KEY = 'cartsKey';
 
+export function addCart(cart) {
+    const carts = getCarts();
+    carts.push(cart);
+    localStorage.setItem(API_KEY, JSON.stringify(carts));
+}
+
+export function getCarts() {
+  return JSON.parse(localStorage.getItem(API_KEY)) ?? [];
 }
